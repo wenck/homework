6587b237a655ecf8050219fec1c8c046eaacede1
wenck <49089816+wenck@users.noreply.github.com>
2021-10-16 22:37:14 +0800

    Create 闰年.c
    题目内容：输入年、月、日三个正整数，判断日期是否合法
    
    输入：三个正整数  
    
    输出： OK或NO
    
    样例1：输入 1200 2 29       输出 OK 
    
    样例2：输入 2018 2 29        输出 NO

---
+++ 闰年.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	unsigned int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+	if (b == 2)
+		if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0))
+			if (c <= 29)
+				printf("OK");
+			else
+				printf("NO");
+		else if (c <= 28)
+			printf("OK");
+		else
+			printf("NO");
+	else if (b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 || b == 12)
+		if (c <= 31)
+			printf("OK");
+		else
+			printf("NO");
+	else if (b == 4 || b == 6 || b == 9 || b == 11)
+		if (c <= 30)
+			printf("OK");
+		else
+			printf("NO");
+
+	return 0;
+}
