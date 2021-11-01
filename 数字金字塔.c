f17e972e7f4c92f1c641dc64919efd2cafaafad2
wenck <49089816+wenck@users.noreply.github.com>
2021-11-01 22:00:19 +0800

    Create 数字金字塔.c

---
+++ 数字金字塔.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void pyramid(int n) {
+	int i, j;
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n - i; j++)
+			printf(" ");
+		for (j = 1; j <= i; j++)
+			printf("%d ", i);
+		putchar('\n');
+	}
+}
+
+int main() {
+	pyramid(5);
+	return 0;
+}
