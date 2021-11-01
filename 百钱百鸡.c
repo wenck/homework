1636a61a291912db21b29f5276c22c42851e945f
wenck <49089816+wenck@users.noreply.github.com>
2021-11-01 21:39:27 +0800

    Update 百钱百鸡.c

--- 百钱百鸡.c
+++ 百钱百鸡.c
@@ -6,7 +6,7 @@ int main() {
 		for (y = 1; y <= 33; y++) {
 			z = 100 - x - y;
 			if (5 * x + 3 * y + z / 3 == 100 && z % 3 == 0)
-				printf("result=%5d %5d %5d\n", x, y, z);
+				printf("result= %-5d %-5d %-5d\n", x, y, z);
 		}
 
 	return 0;
