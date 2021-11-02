6c401299232124983c17b192aa7e5113de6d4b30
wenck <49089816+wenck@users.noreply.github.com>
2021-11-02 09:16:41 +0800

    Update and rename 输出五个数数组最大值.c to 输出5个数字数组最大值.c

--- 输出5个数字数组最大值.c
+++ 输出5个数字数组最大值.c
@@ -4,11 +4,11 @@
 
 int main() {
 	int d[N], max, k, i;
-	for (i = 1; i <= N; i++)
+	for (i = 0; i < N; i++)
 		scanf_s("%d", &d[i]);
 	max = d[0];
 	k = 0;
-	for (i = 0; i < N; i++)
+	for (i = 1; i <= N; i++)
 		if (d[i] > max) { max = d[i]; k = i; }
 	printf_s("max=d[%d]=%d\n", k, max);
 
